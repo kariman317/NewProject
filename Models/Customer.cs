e0ad0a264d77e9211acd35f6a687382d349a1e92
RaniaMohamedHesham <raniaheshamfci40@gmail.com>
2022-03-21 17:59:52 +0200

    Adding layout , puuting render body

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using Restratant.Models;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Restratant
 {
